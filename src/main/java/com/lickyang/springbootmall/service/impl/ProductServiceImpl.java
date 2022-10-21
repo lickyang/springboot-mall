8675eb56a8781de0d1abbecba02596dde02f23ee
lick <lickyang@gmail.com>
2022-10-21 11:16:20 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/lickyang/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/lickyang/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lickyang.springbootmall.service.impl;
 
+import com.lickyang.springbootmall.constant.ProductCategory;
 import com.lickyang.springbootmall.dao.ProductDao;
 import com.lickyang.springbootmall.dto.ProductRequest;
 import com.lickyang.springbootmall.model.Product;
@@ -16,8 +17,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
