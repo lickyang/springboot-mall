e42f82d9a679f41b2face55464fef6f6b14d5466
lick <lickyang@gmail.com>
2022-10-21 12:25:54 +0800

    商品功能 - 實作查詢商品列表（排序）

--- src/main/java/com/lickyang/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/lickyang/springbootmall/dao/impl/ProductDaoImpl.java
@@ -43,6 +43,9 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+
+        sql += " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort() + " ";
+
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
         return productList;
