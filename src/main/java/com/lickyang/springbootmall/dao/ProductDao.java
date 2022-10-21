09a5eb078e27140b85ca668e676176b7350540f1
lick <lickyang@gmail.com>
2022-10-21 10:29:26 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/lickyang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/lickyang/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.lickyang.springbootmall.dao;
 import com.lickyang.springbootmall.dto.ProductRequest;
 import com.lickyang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
